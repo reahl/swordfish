cde36bdd37aa2b4105b6ca1b5d3a98a86d5628c7
Iwan Vosloo <iwan@reahl.org>
2025-09-25 09:45:46 +0200

    Changed to be in the reahl namespace

---
+++ src/reahl/swordfish/__init__.py
@@ -0,0 +1,8 @@
+"""
+Swordfish - A classic Smalltalk IDE for GemStone/Smalltalk
+
+This package provides a Python-based IDE that offers a classic Smalltalk development 
+experience for GemStone/Smalltalk systems.
+"""
+
+__version__ = "1.0.0"
